3958e8d835fb642da014444d98ebeb35810e6f3c
Kapilan M <mnckapilan@gmail.com>
2022-11-15 12:15:07 +0000

    Complete Week 1

--- recfun/src/main/scala/recfun/RecFun.scala
+++ recfun/src/main/scala/recfun/RecFun.scala
@@ -1,5 +1,8 @@
 package recfun
 
+import scala.annotation.tailrec
+import scala.language.postfixOps
+
 object RecFun extends RecFunInterface {
 
   def main(args: Array[String]): Unit = {
@@ -12,7 +15,7 @@ object RecFun extends RecFunInterface {
   }
 
   /**
-   * Exercise 1
+   * Exercise 1ß
    */
   def pascal(c: Int, r: Int): Int = {
     if (c == 0 || r == 0 || c == r) 1
@@ -24,12 +27,13 @@ object RecFun extends RecFunInterface {
    * Exercise 2
    */
   def balance(chars: List[Char]): Boolean = {
-    def balanced(chars: List[Char], count: Int): Boolean = {
-      if (chars.isEmpty) true
-      else if (count < 0) false
-      else if (chars.head == '(') balanced(chars.tail, count + 1)
-      else if (chars.head == ')') balanced(chars.tail, count - 1)
-      balanced(chars.tail, count)
+    @tailrec
+    def balanced(chars: List[Char], open: Int): Boolean = {
+      if (chars.isEmpty) open == 0
+      else if (open < 0) false
+      else if (chars.head == '(') balanced(chars.tail, open + 1)
+      else if (chars.head == ')') balanced(chars.tail, open - 1)
+      else balanced(chars.tail, open)
     }
 
     balanced(chars, 0)
@@ -39,5 +43,13 @@ object RecFun extends RecFunInterface {
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def count(money: Int, coins: List[Int]): Int = {
+      if (money < 0 || coins.isEmpty) 0
+      else if (money == 0) 1
+      else count(money, coins.tail) + count(money - coins.head, coins)
+    }
+
+    count(money, coins)
+  }
 }
